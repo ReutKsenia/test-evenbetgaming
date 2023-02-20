81413d6f18a1d95bdbe61d16fa5d1070a3a96c69
Rieut Ksenia <rieut.ksenia20001@gmail.com>
2023-02-20 12:46:17 +0300

    Template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='Main'>
+        <div>1 block</div>
+        <div>2 block</div>
+      </div>
+      <button className='Start-btn'>START</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
