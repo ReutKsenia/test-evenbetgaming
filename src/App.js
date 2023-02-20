2ecdf326c5e3f3ca4a199290a84458cc38622568
Rieut Ksenia <rieut.ksenia20001@gmail.com>
2023-02-20 15:00:44 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [seconds, setSeconds] = useState(5)
+  const [timerActive, setTimerActive] = useState(false)
+
+  useEffect(() => {
+    if (seconds > 0 && timerActive) {
+      setTimeout(setSeconds, 1000, seconds - 1)
+    } else {
+      setSeconds(5)
+      setTimerActive(false)
+    }
+  }, [seconds, timerActive])
   return (
     <div className='App'>
       <div className='Main'>
-        <div>1 block</div>
-        <div>2 block</div>
+        <div className='Block-1'>
+          <span>1</span>
+          <div className={timerActive ? 'Circle' : 'Circle-hidden'}></div>
+        </div>
+        <div className='Block-2'>2</div>
       </div>
-      <button className='Start-btn'>START</button>
+      <button
+        className='Start-btn'
+        onClick={() => setTimerActive(true)}
+        disabled={timerActive}
+      >
+        {timerActive ? seconds : 'START'}
+      </button>
     </div>
   )
 }
